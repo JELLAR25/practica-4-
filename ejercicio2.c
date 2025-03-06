03085c849f0943c85dbc2992b9b2c0f8890dfef8
JELLAR25 <fotosdiego780@gmail.com>
2025-03-06 14:28:12 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,19 @@
+//Diego Vazquez Cabañas//
+//ejercicio 1//
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+    {
+    printf("a es menor \a");
+    }
+    else
+    {
+      printf("b es mayor o igual a\n");
+    }       
+    return 0;
+}
