92624c017920c8f6e9548e01939ade28ffbea403
JELLAR25 <fotosdiego780@gmail.com>
2025-03-06 14:46:28 -0600

    Create ejercico3.c

---
+++ ejercico3.c
@@ -0,0 +1,37 @@
+//Diego Vazquez Cabañas//
+//ejercicio 1//
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  int res = 0;
+  int i;
+
+  for(i = 0; i< 11; i++)
+  {
+    res= res + i;
+  }
+  printf("solucion 1 - EL numero es: %d\n", res);
+
+  res = 0;
+  for (i = l; i<=10 i++)
+  {
+    res = res + i++
+  }
+  printf("solucion 2 - EL numero es: %d\n",res);
+res =0;
+for(i = 0; i<= 10; i++)
+  {
+    res= res + i;
+  }
+  printf("solucion 3 - EL numero es: %d\n", res);
+  res=0;
+for(i = 0; i<= 10; i++)
+  {
+    res= res + i;
+  }
+  printf("solucion 4 - EL numero es: %d\n", res);
+
+  return 0;
+}
